72721f5848282174880eae293ff5f4d147d5165b
umermemon4648 <umermemon4648@gmail.com>
2023-03-28 18:13:59 +0500

    base url changes

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,5 +1,4 @@
 
 
-
-
-export const API_BASE_URL = 'https://absolute-estore-zeta.vercel.app/';
+// export const API_BASE_URL = 'https://absolute-estore-zeta.vercel.app/';
+export const API_BASE_URL = 'https://ecommerce-mern-app-zeta.vercel.app/';
