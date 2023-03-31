1ecbca92156c139bcc86ba6c052fb721b4628695
umermemon4648 <umermemon4648@gmail.com>
2023-03-31 13:15:28 +0500

    commit

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use('/api/payment',paymentRoutes)
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', '/index.html'));
 });
 
 
