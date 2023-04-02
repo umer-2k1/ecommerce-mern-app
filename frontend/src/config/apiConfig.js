8536b428a9afbbea2ee974be534f0042a7e1115b
umermemon4648 <umermemon4648@gmail.com>
2023-04-02 12:47:14 +0500

    made changes in api url

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -3,4 +3,4 @@
 
 // export const API_BASE_URL = 'http://localhost:5000';
 // export const API_BASE_URL = 'https://shy-teal-bison-slip.cyclic.app';
-export const API_BASE_URL = 'https://absolute-estore.cyclic.app/';
+export const API_BASE_URL = 'https://absolute-estore.cyclic.app';
