7f1eee0ca3e9f5a49568d8a7a6107e5a2dc782b8
umermemon4648 <umermemon4648@gmail.com>
2023-04-16 14:23:40 +0500

    refactor contact page

--- frontend/src/components/Home/ContactUs.js
+++ frontend/src/components/Home/ContactUs.js
@@ -5,56 +5,27 @@ import { BsLinkedin } from 'react-icons/bs';
 const ContactUs = () => {
   return (
     <>
-   <div className="contain">
-
-<div className="wrapper">
-
-  <div className="form">
-    <h4>GET IN TOUCH</h4>
-    <h2 className="form-headline">Send us a message</h2>
-    <form id="submit-form">
-      <p>
-        <input id="name" className="form-input" type="text" placeholder="Your Name*"/>
-        <small className="name-error"></small>
-      </p>
-      <p>
-        <input id="email" className="form-input" type="email" placeholder="Your Email*"/>
-        <small className="name-error"></small>
-      </p>
-      <p className="full-width">
-        <input id="company-name" className="form-input" type="text" placeholder="Company Name*" required/>
-        <small></small>
-      </p>
-      <p className="full-width">
-        <textarea  minLength="20" id="message" cols="30" rows="7" placeholder="Your Message*" required></textarea>
-        <small></small>
-      </p>
-      <p className="full-width">
-        <input type="checkbox" id="checkbox" name="checkbox" defaultChecked/> Yes, I would like to receive communications by call / email about Company's services.
-      </p>
-      <p className="full-width">
-        <input type="submit" className="submit-btn" value="Submit"/>
-        <button className="reset-btn">Reset</button>
-      </p>
-    </form>
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+      <form action="#" class="space-y-8">
+          <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+          </div>
+          <div>
+              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required/>
+          </div>
+          <div class="sm:col-span-2">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          </div>
+          <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+      </form>
   </div>
-
-  <div className="contacts contact-wrapper">
-
-    <ul>
-      <li><span className="highlight-text-grey">If you have any questions regarding your order, feel free to send email, call or Whatsapp us on our support number</span></li>
-      <span className="hightlight-contact-info">
-        <li className="email-info"><i className="fa fa-envelope" aria-hidden="true"></i> umermemon4648@gmail.com</li>
-        
-        <div className='flex'>
-        <li><BsLinkedin/> <span className="highlight-text">https://www.linkedin.com/in/m-umer-memon/</span></li>
-        </div>
-
-      </span>
-    </ul>
-  </div>
-</div>
-</div>
+</section>
     </>
   )
 }
